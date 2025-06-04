3fb43dcfc988e0933968f889288fd24791db5336
gunsinphotos <photos54516@gmail.com>
2025-06-04 20:52:41 +0545

    Initial commit

--- model.py
+++ model.py
@@ -1,15 +1,25 @@
+from typing import List, Optional
+
+from fastapi import Form
 from pydantic import BaseModel
 
 
 class Todo(BaseModel):
-    id: int
+    id: Optional[int]
     item: str
+
+    @classmethod
+    def as_form(
+            cls,
+            item: str = Form(...)
+    ):
+        return cls(item=item)
 
     class Config:
         schema_extra = {
             "example": {
                 "id": 1,
-                "item": "Example Schema!"
+                "item": "Example schema!"
             }
         }
 
@@ -21,5 +31,23 @@ class TodoItem(BaseModel):
         schema_extra = {
             "example": {
                 "item": "Read the next chapter of the book"
+            }
+        }
+
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
             }
         }
